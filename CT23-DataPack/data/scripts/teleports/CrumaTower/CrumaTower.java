ee5f1c64ed0c903f59847971a64165bd64152c76
BossForever <BossForever@117d2afa-4b10-4f97-ac9f-ac0015ab88bf>
2014-06-11 21:25:12 +0000

    Correção Cruma Tower Teleports e Survivor

--- CT23-DataPack/data/scripts/teleports/CrumaTower/CrumaTower.java
+++ CT23-DataPack/data/scripts/teleports/CrumaTower/CrumaTower.java
@@ -1,35 +1,42 @@
 package teleports.CrumaTower;
 
+import ct23.xtreme.gameserver.model.Location;
 import ct23.xtreme.gameserver.model.actor.L2Npc;
 import ct23.xtreme.gameserver.model.actor.instance.L2PcInstance;
 import ct23.xtreme.gameserver.model.quest.Quest;
-import ct23.xtreme.gameserver.model.quest.QuestState;
 
 public class CrumaTower extends Quest
 {
-	public CrumaTower(int questId, String name, String descr)
+	// NPC
+	private static final int MOZELLA = 30483;
+	// Locations
+	private static final Location TELEPORT_LOC1 = new Location(17776, 113968, -11671);
+	private static final Location TELEPORT_LOC2 = new Location(17680, 113968, -11671);
+	// Misc
+	private static final int MAX_LEVEL = 55;
+	
+	public CrumaTower()
 	{
-		super(questId, name, descr);
-		addStartNpc(30483);
-		addTalkId(30483);
+		super(-1,"Cruma Tower", "teleports");
+		addFirstTalkId(MOZELLA);
+		addStartNpc(MOZELLA);
+		addTalkId(MOZELLA);
 	}
 	
+	
 	@Override
-	public String onTalk (L2Npc npc, L2PcInstance player)
+	public String onTalk(L2Npc npc, L2PcInstance player)
 	{
-		QuestState st = player.getQuestState(getName());
-		String htmltext = "";
-		
-		if (player.getLevel() > 55)
-			htmltext = "30483.htm";
-		else
-			player.teleToLocation(17724,114004,-11672);
-		st.exitQuest(true);
-		return htmltext;
+		if (player.getLevel() <= MAX_LEVEL)
+		{
+			player.teleToLocation(getRandomBoolean() ? TELEPORT_LOC1 : TELEPORT_LOC2, false);
+			return null;
+		}
+		return "30483-1.htm";
 	}
 
 	public static void main(String[] args)
 	{
-		new CrumaTower(-1, "CrumaTower", "teleports");
+		new CrumaTower();
 	}
 }
